99f328056fa3523b32cd484cb0f55707c8344c6a
DaeHyun_Woo <po668312@naver.com>
2023-02-03 21:45:26 +0900

    feat: ✨make redirects, rewrites for hide API_KEY

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,11 +5,45 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/about"}>About</Link>
+      <img src="/vercel.svg" />
+      {/* public에 들어있는건 그냥 확장자명만 작성하면 됨! 편리함 */}
+      <div>
+        <a href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
+        </a>
+
+        <a
+          href="/about"
+          className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </a>
+      </div>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import Seo from "components/Seo";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <>
       <Seo title={"home"} />
+      {!movies && <h4>Loading....</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
       <div>
         <h1>Hello</h1>
       </div>
