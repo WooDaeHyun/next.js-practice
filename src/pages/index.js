898b157a934b7b734ed43b173ec19247b789dc47
DaeHyun_Woo <po668312@naver.com>
2023-02-03 23:24:38 +0900

    feat: ✨Server Side Rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,13 @@
 import { useState, useEffect } from "react";
 import Seo from "components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
 
   return (
     <>
       <Seo title={"home"} />
-      {!movies && <h4>Loading....</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
@@ -26,3 +18,18 @@ export default function Home() {
     </>
   );
 }
+
+//이 이름은 꼭 지켜져야 함!
+//어떤 코드를 안에 집어 넣든 이 안의 코드는 그 코드는 server에서 돌아가게 될 것이다!!
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
+
+//URL -> ABOUT.JS DEFAULT 컴포넌트 확인 -> _APP.js ->
