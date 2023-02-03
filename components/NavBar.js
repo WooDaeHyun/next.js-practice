70ea0fd989069fb75f7ece0bf31bef09319573a2
DaeHyun_Woo <po668312@naver.com>
2023-02-03 17:56:34 +0900

    feat: ✨make styled jsx in component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,19 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href={"/"} className={router.pathname === "/" ? styles.active : ""}>
-        Home
-      </Link>
-      <Link
-        href={"/about"}
-        className={router.pathname === "/about" ? styles.active : ""}
-      >
-        About
-      </Link>
+      <Link href={"/"}>Home</Link>
+      <Link href={"/about"}>About</Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+      `}</style>
     </nav>
   );
 }
+
+//styled jsx는 next.js 고유의 방법이라고 할 수 있음
