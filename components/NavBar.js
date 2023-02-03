b051fdb67e0fdb898d8359b4bb0194fd7f029986
DaeHyun_Woo <po668312@naver.com>
2023-02-03 20:02:43 +0900

    feat: ✨global css and custom App component

--- components/NavBar.js
+++ components/NavBar.js
@@ -17,3 +17,6 @@ export default function NavBar() {
 }
 
 //styled jsx는 next.js 고유의 방법이라고 할 수 있음
+//style의 적용 범위는 해당 컴포넌트 내에서만 적용된다. 스코프가 해당 지금 컴포넌트로 한정됨!!!
+//그래서 글로벌 css를 적용하기 위해서는 다른 방법을 찾아봐야함! _app.js 주석 참고
+//NavBar는 라우터 역할
---
+++ src/pages/_app.js
@@ -0,0 +1,22 @@
+import NavBar from "components/NavBar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
+}
+
+// 이건 규칙이다 _app을 통해서 App 컴포넌트를 컨트롤하는 것이고,
+// 글로벌 스타일을 적용하거나 모든 컴포넌트에 공통적으로 적용할 컴포넌트는 여기서 붙이는 것이다. 프레임워크의 규칙이다!
+// 그리고 글로벌 css는 일반 컴포넌트에서 붙이면 error가 발생하고 오로지 _app컴포넌트 내에서만 불러올 수 있다!!!!!
+// 그래서 .module.css 가 중요한 이유가 이것이다.
+// 일반 컴포넌트 내에서 css 파일을 불러오기 위해서는 확장자 명이 .module.css여야 한다!
